06a337ee2baab3ac7a0f4d0e722f0c2cad21e0b5
Nico Dann <nicodann@gmail.com>
2021-12-15 19:36:37 +0000

    Create function in client.js to dynamically populate tweets to page.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,21 +3,82 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData =   {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+// const tweet =   {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1639316897884
+// };
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1639316897884
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1639316897884
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1639403297884
+  }
+];
 
-const createTweetElement = () => {
+$(document).ready(() => {
 
-  
+  const renderTweets = function(tweets) {
 
-};
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('section.tweets').append($tweet);
+    }
 
+  };
+
+  const createTweetElement = function(tweet) {
+
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div class="avatarName">
+            <img src="${tweet.user.avatars}" alt="Avatar Picture">
+            <p>${tweet.user.name}</p>
+          </div>
+          <p class="handle">${tweet.user.handle}</p>
+        </header>
+        <p class="tweet-content">${tweet.content.text}</p>
+        <footer>
+          <p>${tweet.created_at}</p>
+          <div class="icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+
+    return $tweet;
+
+  };
+
+  renderTweets(data);
+
+
+});
